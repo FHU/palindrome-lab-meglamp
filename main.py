99f2f372ba83f86aade720e9aa43542d24c8cd65
meglamp <megan.lampley@students.fhu.edu>
2024-02-06 12:23:44 -0600

    main.py
    all blank strings fail

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
+    word = word.strip()
     if word == "":
         return(False)
     else:
-        word = word.strip()
         length = len(word)
         wordy = []
         for i in range((length),0,-1):
             wordy.append(word[i-1])
         separator = ''
         backwards = separator.join(wordy)
-        return(str(backwards.lower() == word.lower()))
+        return(backwards.lower() == word.lower())
 
 
 
